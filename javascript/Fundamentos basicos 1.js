7045f05e9176d768828d10eb6143a7ef281cadff
BraianLopez <55114281+BraianLopez@users.noreply.github.com>
2023-08-16 03:01:46 -0300

    acerca de python-css

--- javascript/Fundamentos basicos 1.js
+++
@@ -1,9 +0,0 @@
-//1 a 255
-function unoADosCincuentaYCinco(){
-    for(i=0;i<256;i++){
-        console.log(i);
-        return i;
-    }
-}
-unoADosCincuentaYCinco = unoADosCincuentaYCinco();
-console.log(unoADosCincuentaYCinco);
